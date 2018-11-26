e4c0abb7302f9b47e452e1dee56bada4ef314c88
crysn <crysnat@mail.ru>
2018-11-26 17:37:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/crysn/goodgame/StudentMainMenu.java
@@ -0,0 +1,12 @@
+package com.example.crysn.goodgame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StudentMainMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_main_menu);
+    }
+}
---
+++ app/src/main/java/com/example/crysn/goodgame/TeacherMainMenu.java
@@ -0,0 +1,12 @@
+package com.example.crysn.goodgame;
+
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+
+public class TeacherMainMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_main_menu);
+    }
+}
