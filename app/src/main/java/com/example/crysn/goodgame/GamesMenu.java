0ae34b45241169e539cfd67cbeead1ea1b09618e
crysn <crysnat@mail.ru>
2018-12-01 01:49:10 +0700

    Added Game menu (for testing return back to level menu)

---
+++ app/src/main/java/com/example/crysn/goodgame/GamesMenu.java
@@ -0,0 +1,12 @@
+package com.example.crysn.goodgame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class GamesMenu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games_menu);
+    }
+}
--- app/src/main/java/com/example/crysn/goodgame/LevelsMenu.java
+++ app/src/main/java/com/example/crysn/goodgame/LevelsMenu.java
@@ -1,7 +1,9 @@
 package com.example.crysn.goodgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LevelsMenu extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class LevelsMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels_menu2);
+    }
+
+    public void pressLevel1(View view) {
+        Intent intent = new Intent(this, GamesMenu.class);
+        startActivity(intent);
+    }
+
+    public void pressLevel2(View view) {
+        Intent intent = new Intent(this, GamesMenu.class);
+        startActivity(intent);
+    }
+
+    public void pressLevel3(View view) {
+        Intent intent = new Intent(this, GamesMenu.class);
+        startActivity(intent);
     }
 }
