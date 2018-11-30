f121ce1266c9e76bacdf4f8c68efb5c7c5db1571
Анна Мирянова <a.mirianova@g.nsu.ru>
2018-11-30 19:54:26 +0300

    added levels menu

--- app/src/main/java/com/example/crysn/goodgame/StudentMainMenu.java
+++ app/src/main/java/com/example/crysn/goodgame/StudentMainMenu.java
@@ -1,12 +1,26 @@
 package com.example.crysn.goodgame;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class StudentMainMenu extends AppCompatActivity {
+    Button toLevels;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_main_menu);
+        toLevels = findViewById(R.id.levels);
+        toLevels.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(v.getContext(), LevelsMenu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
